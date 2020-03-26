6489bc9c3775663b4c87db94aeefd7e9d28d370a
Marco Argentieri <marco@vulpem.com>
2020-03-26 22:07:48 +0100

    Use ested commands for market price and list

--- src/index.ts
+++ src/index.ts
@@ -34,14 +34,14 @@ program
     info('=========*** Info ***==========\n')
 
     const { market, provider } = state.get();
-    
+
     if (provider.selected)
       log(`Endpoint: ${provider.endpoint}`);
 
     if (market.selected)
       log(`Market: ${market.pair}`);
 
-    
+
   })
 /**
  * Connect
@@ -54,10 +54,10 @@ program
 
     if (!isValidUrl(endpoint))
       return error('The provided endpoint URL is not valid');
-    
+
     // TODO: Connect to provided endpoint and fetch the available pairs along with his pubkey
     const pairs = ['LBTC-USDT', 'LBTC-EQUI'];
-    state.set({ provider: { endpoint, pairs, selected: true }});
+    state.set({ provider: { endpoint, pairs, selected: true } });
 
     return log(`Current provider endpoint: ${endpoint}`)
   });
@@ -65,36 +65,55 @@ program
 /**
  * Market
  */
-program
-  .command('market [pair]')
-  .option('-ls, --list', 'Get available assets pairs for the selected provider')
+
+const market = program
+  .command('market <pair>')
   .description('Select the default asset pair to use for the swap')
-  .addHelpCommand(false)
-  .action((pair, cmdObj) => {
+  .action((pair) => {
     info('=========*** Market ***==========\n');
 
     const { provider, market } = state.get();
     if (!provider.selected)
       return error('A provider is required. Select with connect <endpoint> command');
 
-    if (cmdObj.list)
-      return provider.pairs.forEach(p => log(p))
-
-    if (!pair)
-      return error('An asset pair is required. Get all the avilable ones with the --list option');
-
+    /*    if (cmdObj.list)
+         return provider.pairs.forEach(p => log(p))
+    */
+    /*    if (!pair)
+         return error('An asset pair is required. Get all the avilable ones with the --list option');
+    */
     if (!provider.pairs.includes(pair))
       return error('Pair not suppported by the selcted provider');
 
     if (!market.selected && market.pair !== pair)
-      state.set({ market: { selected: true, pair }});
-    
+      state.set({ market: { selected: true, pair } });
+
     //TODO: Fetch the price from the daemon
     log(`Current market: ${pair}`);
-    log();
     success(`Price: 1 asset_a is equal to X asset_b at timestamp`);
   });
 
+market
+  .command('list')
+  .description('Get available assets pairs for current provider')
+  .action(() => {
+    info('=========*** Market ***==========\n');
+
+    const { provider } = state.get();
+    provider.pairs.forEach(p => log(p))
+  })
+
+market
+  .command('price')
+  .description('Get the current price for the selected market')
+  .action(() => {
+    info('=========*** Market ***==========\n');
+
+    const { market } = state.get();
+
+    log(`Current market: ${market.pair}`);
+    log(`Price: 1 asset_a is equal to X asset_b at timestamp`);
+  })
 
 /**
  * Wallet
