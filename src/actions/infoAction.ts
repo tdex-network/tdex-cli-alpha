2026fdce57facacf37952db14c633e4de39edbff
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2020-07-10 14:06:42 +0200

    info command - Message when state is empty

--- src/actions/infoAction.ts
+++ src/actions/infoAction.ts
@@ -6,11 +6,15 @@ const state = new State();
 export default function () {
   info('=========*** Info ***==========\n');
 
-  const { market, provider, network } = state.get();
+  const { market, provider, network, wallet, operator } = state.get();
 
   if (network.selected) log(`Network: ${network.chain}`);
 
-  if (provider.selected) log(`Endpoint: ${provider.endpoint}`);
+  if (provider.selected) log(`Provider endpoint: ${provider.endpoint}`);
 
   if (market.selected) log(`Market: ${market.pair}`);
+
+  if (wallet.selected) log(`Wallet address: ${wallet.address}`);
+
+  if (operator.selected) log(`Operator endpoint: ${operator.endpoint}`)
 }
